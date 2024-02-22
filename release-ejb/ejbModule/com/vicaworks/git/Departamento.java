aee19837dbc9b77a918179efe1e6fe4bff806825
vanessanasimba <nmvanessa25@gmail.com>
2024-02-22 14:22:00 -0500

    sss

---
+++ release-ejb/ejbModule/com/vicaworks/git/Departamento.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.vicaworks.git;
+
+/**
+ * @author cristianvillarreal
+ *
+ */
+public class Departamento {
+
+	private String nombre;
+	private String identificador;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getIdentificador() {
+		return identificador;
+	}
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+}
