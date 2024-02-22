b8b74f27c08a0c4e0499a2851eecce887b907872
Vanessa Nasimba <nmvanessa25@gmail.com>
2024-02-22 11:41:11 -0500

    vn

---
+++ release-ejb/ejbModule/com/vicaworks/git/Carro.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.vicaworks.git;
+
+/**
+ * @author cristianvillarreal
+ *
+ */
+public class Carro {
+	
+	private String placa;
+	private String marca;
+	
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+
+}
