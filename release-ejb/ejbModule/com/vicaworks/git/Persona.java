92f82334a638fe1051cb0e9ce32c02920bdd2e44
vanessanasimba <nmvanessa25@gmail.com>
2024-02-22 14:10:43 -0500

    vn - cambio

---
+++ release-ejb/ejbModule/com/vicaworks/git/Persona.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.vicaworks.git;
+
+/**
+ * @author cristianvillarreal
+ *
+ */
+public class Persona {
+	
+	private String nombre;
+	private String identificacion;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getIdentificacion() {
+		return identificacion;
+	}
+	public void setIdentificacion(String identificacion) {
+		this.identificacion = identificacion;
+	}
+
+}
